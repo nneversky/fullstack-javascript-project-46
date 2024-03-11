72aab8d669f136fce53c641f5a540cb6fabcec7c
nneversky <skeleton.ubica@gmail.com>
2024-03-11 17:00:21 +0300

    readFile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
 // import genDiff from '@hexlet/code';
 
-const findDirectory = require('../src/parse');
-const { program } = require('commander');
+const readFileOnDirectory = require('../src/parse')
+const { program } = require('commander')
 
 program
-  .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => {
-    const arrFilepathUser = [filepath1, filepath2];
-    return ([firstJsonFile, secondJsonFile] = arrFilepathUser);
-  });
-program.parse(process.argv);
+    .version('1.0.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .action((filepath1, filepath2) => {
+        const arrFilepathUser = [filepath1, filepath2]
+        return [firstJsonFile, secondJsonFile] = arrFilepathUser
+    })
+program.parse(process.argv)
 
-const pathToFirstFile = findDirectory(firstJsonFile)
-const pathToSecondFile = findDirectory(secondJsonFile)
+const oneFile = readFileOnDirectory(firstJsonFile)
+const twoFile = readFileOnDirectory(secondJsonFile)
 
-console.log(pathToFirstFile);
-console.log(pathToSecondFile);
--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,12 @@
-module.exports = findDirectory = (nameFile) => {
-  const path = require('node:path');
-  const directoryParent =
-    path.resolve(process.cwd(), '../') + '\\__fixtures__\\' + nameFile;
-  return directoryParent;
-};
+module.exports = readFileOnDirectory = (nameFile) => {
+    const path = require('node:path')
+    const fs = require('fs')
+    const pathOnDirectory = path.resolve(process.cwd(), '../') + '\\__fixtures__\\' + nameFile
+    fs.readFile(pathOnDirectory, 'utf8', (err, data) => {
+        if (err) {
+            console.error('File not found!!!')
+            return
+        }
+        console.log(data)
+    })
+}
