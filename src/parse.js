43960ca30aa95debad604b09c949eb2c4fa9ebcc
nneversky <skeleton.ubica@gmail.com>
2024-03-18 20:38:11 +0300

    ref parse

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,13 @@
 import path from 'node:path'
 import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default (nameFile) => {
     const pathOnDirectory =
-        path.resolve(process.cwd(), '../') + '\\__fixtures__\\' + nameFile
+        path.resolve(__dirname, '__fixtures__', nameFile).replace(/src\\/, '');
     try {
         const data = readFileSync(pathOnDirectory, 'utf8')
         const parsedDataJson = JSON.parse(data)
