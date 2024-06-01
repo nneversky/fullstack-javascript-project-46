93b14d13bda6be7727d6e37a8cd8e67d2f3055ba
nneversky <skeleton.ubica@gmail.com>
2024-06-01 16:17:17 +0300

    ref 01.06

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import { readFileSync } from 'fs'
 export const gendiff = (oneFileName, twoFileName) => {
     const objProp = {}
 
-    // const absolutePath1 = resolve(
-    //     process.cwd(),
-    //     `../__fixtures__/${oneFileName}`
-    // )
     const absolutePath1 = resolve(process.cwd(), oneFileName)
     const ext1 = extname(absolutePath1)
     const filedata1 = readFileSync(absolutePath1, 'utf-8')
 
-    // const absolutePath2 = resolve(
-    //     process.cwd(),
-    //     `../__fixtures__/${twoFileName}`
-    // )
     const absolutePath2 = resolve(process.cwd(), twoFileName)
     const ext2 = extname(absolutePath2)
     const filedata2 = readFileSync(absolutePath2, 'utf-8')
