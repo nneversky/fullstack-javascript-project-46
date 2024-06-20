e5dbd10b615d2727a5e6b7c1720279d65ce0d008
nneversky <skeleton.ubica@gmail.com>
2024-06-20 19:59:51 +0300

    new ref 20.1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,20 @@
 import { test, expect } from '@jest/globals'
+import path, { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { readFileSync } from 'fs'
 import { gendiff } from '../src/index.js'
 
-test('testExt', () => {
-    const diff = gendiff(
-        '__fixtures__/filepath1.json',
-        '__fixtures__/filepath2.json'
-    )
-    expect(diff[0].ext).toEqual('.json')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const getFixturePath = (filepath) =>
+    path.join(__dirname, '..', '__fixtures__', filepath)
+
+test('testJsonFormat', () => {
+    const pathJsonFormat = resolve(process.cwd(), '__fixtures__/resultJson.txt')
+    const jsonFormatData = readFileSync(pathJsonFormat, 'utf-8')
+    const filepath1 = getFixturePath('filepath1.json')
+    const filepath2 = getFixturePath('filepath2.json')
+    const result = gendiff(filepath1, filepath2, 'json')
+    expect(result).toEqual(jsonFormatData)
 })
