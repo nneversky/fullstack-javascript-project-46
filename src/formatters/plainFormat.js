9ef87222f8e28e152fc511f9e64205b332ca33bc
nneversky <skeleton.ubica@gmail.com>
2024-06-22 11:47:20 +0300

    ref plainFormat

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -6,24 +6,27 @@ export const plainFormat = (arr, parentKey = '') => {
             const formattedValue =
                 typeof value.value === 'object' && value.value !== null
                     ? '[complex value]'
-                    : JSON.stringify(value.value)
+                    : `'${JSON.stringify(value.value).slice(1, JSON.stringify(value.value).length-1)}''`
             return `Property '${fullKey}' was added with value: ${formattedValue}`
             ///
-        } if (value.status === 'removed') {
+        }
+        if (value.status === 'removed') {
             return `Property '${fullKey}' was removed`
             ///
-        } if (value.status === 'changed') {
+        }
+        if (value.status === 'changed') {
             const formattedOldValue =
                 typeof value.oldValue === 'object' && value.oldValue !== null
                     ? '[complex value]'
-                    : JSON.stringify(value.oldValue)
+                    : `'${JSON.stringify(value.oldValue).slice(1, JSON.stringify(value.oldValue).length-1)}'`
             const formattedNewValue =
                 typeof value.newValue === 'object' && value.newValue !== null
                     ? '[complex value]'
-                    : JSON.stringify(value.newValue)
+                    : `'${JSON.stringify(value.newValue).slice(1, JSON.stringify(value.newValue).length-1)}'`
             return `Property '${fullKey}' was updated. From ${formattedOldValue} to ${formattedNewValue}`
             ///
-        } if (value.status === 'nested') {
+        }
+        if (value.status === 'nested') {
             return plainFormat(value.children, fullKey)
         }
         return []
