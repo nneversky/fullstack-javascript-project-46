c97d8b4ad3b9e8011c8488df88be423d1effdbb3
nneversky <skeleton.ubica@gmail.com>
2024-06-23 10:27:13 +0300

    new prettier

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,14 @@ import { stylishFormat } from './stylishFormat.js';
 import { plainFormat } from './plainFormat.js';
 
 export default (astTree, format) => {
-    switch (format) {
-        case 'stylish':
-            return stylishFormat(astTree);
-        case 'plain':
-            return plainFormat(astTree);
-        case 'json':
-            return JSON.stringify(astTree);
-        default:
-            throw new Error(`Invalid format - ${format}`);
-    }
+  switch (format) {
+    case 'stylish':
+      return stylishFormat(astTree);
+    case 'plain':
+      return plainFormat(astTree);
+    case 'json':
+      return JSON.stringify(astTree);
+    default:
+      throw new Error(`Invalid format - ${format}`);
+  }
 };
